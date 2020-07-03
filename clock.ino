5955bc70135c94c0a4f9b533b6dac1be535639fa
Davidevice <43323298+Davidevice@users.noreply.github.com>
2020-07-03 16:59:21 +0200

    Update clock.ino

--- clock.ino
+++ clock.ino
@@ -1,4 +1,6 @@
 //json versione 5.13  VERSIONE SCHEDA WEMOS
+// questo va, madonna se va!!!
+#include <NTPClient.h>
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
@@ -12,43 +14,53 @@
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
 #include <SPI.h>
+
+
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 #define MAX_DEVICES 4
 #define CLK_PIN   D2
 #define DATA_PIN  D4
 #define CS_PIN    D3
-#include <NTPClient.h>
+
+
+
 int Offset = 7200;   //3600 = 1h
 int ctrlMessaggio = 0;
-int ctrlUScita = 0;
+int ctrlUScita = 0;   // serve per ignorare il messaggio mandato dal pulsante tastiera
 char* newMessageOra;
 String strDaOra;
 String payload;
 bool oraTime;
-//long interval  = 10000;
-//long previousMillis = 0;
 long current;
+
+
 MD_Parola P = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "0.it.pool.ntp.org", Offset, 60000);
-//(curMessage, scrollAlign, scrollSpeed, scrollPause, scrollEffect, scrollEffect);
-uint8_t scrollSpeed = 25;    // default frame delay value
+
+//VARIABILI DISPLAY
+
+uint8_t scrollSpeed = 23;
 textEffect_t scrollEffect = PA_SCROLL_LEFT;
 textPosition_t scrollAlign = PA_LEFT;
-uint16_t scrollPause = 0; // in milliseconds
+uint16_t scrollPause = 0;
 
-// Global message buffers shared by Serial and Scrolling functions
-#define  BUF_SIZE  512
+
+#define  BUF_SIZE  1024
 
 char char_array;
-char curMessage[BUF_SIZE] = { " o_O   +_+    *-*   :_:  " };
+char curMessage[BUF_SIZE] = { "  o_O    +_+    *-*  " };
 char newMessage[BUF_SIZE] = { " MANDA UN MESSAGGIO SU TELEGRAM " };
 bool newMessageAvailable = true;
+
+#define SIMPLE_CALLBACK  "Simple"
+
 CTBot myBot;
-CTBotInlineKeyboard myKbd;
+CTBotReplyKeyboard myKbd;
+bool isKeyboardActive;
 WiFiClient client;
 #define HOSTNAME "Telegram BOt"
-String token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+String token = "1180054157:AAHSy0hngOFOborwhvLVmkLBVNgJXjPH--8";
 
 long previousMillis = 0;
 long interval = 10000;
@@ -83,17 +95,30 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   timeClient.begin();
-  //  myKbd.addButton("OFF", LIGHT_OFF_CALLBACK, CTBotKeyboardButtonQuery);
 
-
-  // set the telegram bot token
   myBot.setTelegramToken(token);
 
-  // check if all things are ok
   if (myBot.testConnection())
     Serial.println("TEST CTBOT OK");
   else
     Serial.println("TEST CTBO KO");
+
+
+
+  myKbd.addButton("\U0001F551 Orologio");
+  myKbd.addButton("\U00002753 Stato");
+  // myKbd.addButton("Location request", CTBotKeyboardButtonLocation);
+  myKbd.addRow();
+  // add a button that send a message with "Hide replyKeyboard" text
+  // (it will be used to hide the reply keyboard)
+  myKbd.addButton("\U0001F6E0 - Officina Vicenzi -");
+  // resize the keyboard to fit only the needed space
+  myKbd.enableResize();
+  isKeyboardActive = false;
+
+
+
+
   P.begin();
   P.setIntensity(1);
   //P.setInvert(true);
@@ -107,26 +132,25 @@ void loop() {
   ArduinoOTA.handle();
 
 
-
-
   if (P.displayAnimate())
   {
+
     if (newMessageAvailable)
     {
       strcpy(curMessage, newMessage); Serial.println("eccolo");
       newMessageAvailable = false;
     }
+
     P.displayReset();
     Serial.println("eccomi qui");
-    Serial.print(newMessage);                                                        //////////
-    P.displayText(newMessage, scrollAlign, scrollSpeed, scrollPause, scrollEffect, scrollEffect);    /////////
-    P.displayAnimate();                                                                               ///////
+    Serial.print(newMessage);
+    P.displayText(newMessage, scrollAlign, scrollSpeed, scrollPause, scrollEffect, scrollEffect);
+    P.displayAnimate();
     unsigned long currentMillis = millis();
 
     if (currentMillis - previousMillis > interval) {
-      // save the last time you blinked the LED
       previousMillis = currentMillis;
-      Serial.println("sto avv messaggio");
+      Serial.println("sto avviando messaggio  ");
       messaggio();
 
     }
@@ -134,40 +158,61 @@ void loop() {
   }
 }
 void ora()  {
-  //while (oraTime == true){
-
+  Serial.print(" ora time in ora()  "); Serial.println(oraTime);
+  TBMessage msg;
+  oraTime = true;  //////////////////////////////////////////////////////////////////////////////////////
   while (oraTime == true) {
-    TBMessage msg;
+    // TBMessage msg;  lo sposto allinizio
     timeClient.update();
     String oraStr = (timeClient.getFormattedTimeNoSeconds());
     P.print(" " + (oraStr));
     delay(5000);
 
-    int a = ((myBot.getNewMessage(msg)));
-    strDaOra = msg.text;//Serial.print(" a   "); Serial.println((myBot.getNewMessage(msg)));
+    int a = ((myBot.getNewMessage(msg)));  // se c'è un messaggio a == 1
+    strDaOra = msg.text; 
 
 
     if (a == 1) {
-      myBot.sendMessage(msg.sender.id, "Esco da orologio");   /////////////////////////////////////////
 
-      TBMessage msg; ////////////////////////////////////////////////
-      Serial.println(" esco da orologio");
-      oraTime = false;
-      a = 0;
-      ctrlMessaggio = 1;
-      newMessageAvailable = true;
-      //return;// messaggio();// newMessageAvailable = true; ///////////////////
-      Serial.print("str in ora    "); Serial.println(strDaOra);
-    messaggio();
+
+      if (msg.text.equalsIgnoreCase("ud83dudee0 - Officina Vicenzi -")) {
+        myBot.sendMessage(msg.sender.id, "sta scorrendo il messaggio predefinito!");
+        myBot.sendMessage(msg.sender.id, "\U0001F6E0 'OFFICINA VICENZI'");
+        ctrlUScita = 1;
+        strDaOra = "-- OFFICINA VICENZI --";
+      }
+      if (msg.text.equalsIgnoreCase("u2753 Stato")) {   ///era else if
+        // Serial.print(" ora time"); Serial.println(oraTime);
+
+        myBot.sendMessage(msg.sender.id, (F("Sto visualizzando l'orologio. ")));
+        myBot.sendMessage(msg.sender.id, "A proposito ... sono le : " + String(oraStr));
+        msg.text = "";
+        oraTime = true;
+        ora;
+      }
+ else     if (msg.text.equalsIgnoreCase("ud83dudd51 Orologio")) {
+        myBot.sendMessage(msg.sender.id, (F("Sei gia' nell'orologio")));
+        msg.text = "";
+        oraTime = true;
+        ora;
+      }
+
+      else {
+        Serial.println(" esco da orologio");
+        oraTime = false;
+        a = 0;
+        ctrlMessaggio = 1;
+        newMessageAvailable = true;
+        Serial.print("str in ora    "); Serial.println(strDaOra);
+        messaggio();
+      }
     }
 
   }
-
 }
-
 void messaggio() {
   Serial.print("ora time    "); Serial.println(oraTime);
-
+  //TBMessage msg;
   if (oraTime == false) { //if (oraTime == false){
 
 
@@ -184,73 +229,128 @@ void messaggio() {
 
       newMessageAvailable = true;
       ctrlUScita = 1;
+      ////////////////////////////////
       return;
     }
 
 
-    TBMessage msg;
+    TBMessage msg;   //// lo sposto allinizio TBMessage msg;
     if (myBot.getNewMessage(msg)) {
 
       if (msg.text.equalsIgnoreCase("ora")) {
         myBot.sendMessage(msg.sender.id, "Attivo orologio, digita qualcosa per uscire e mostrare il messaggio!");
         oraTime = true; ora();
-        // notify the sender
       }
 
-      if (msg.text.equalsIgnoreCase("TEST")) {
-        myBot.sendMessage(msg.sender.id, "TEST OK");
-       return;
-       }
-     
+      if (msg.text.equalsIgnoreCase("u2753 Stato")) {
+        Serial.print(" ora time"); Serial.println(oraTime);
+        if (oraTime == 0) {
+          myBot.sendMessage(msg.sender.id, "Sta passando il msg: " + String(newMessage));
+          return;
+        }
+        if (oraTime == 1) {
+          myBot.sendMessage(msg.sender.id, "Sono in orologio");
+          return;
+        }
+      }
+
+      if (msg.text.equalsIgnoreCase("/start")) {
+        myBot.sendMessage(msg.sender.id, "Ciao, vuoi mandare in scrolling un messaggio?! Scrivilo semplicemente e invialo. "
+                          " Per mostrare la tastiera digita 'tastiera'. "
+                          " Il tasto 'Stato' ti informa cosa c'è al momento sul display. "
+                          " Il tasto in basso fa passare il messaggio predefinito. "
+                          " - by Davidevice - ");
+        return;
+      }
+
+
       if (msg.text.equalsIgnoreCase("aiuto")) {
         myBot.sendMessage(msg.sender.id, "Scrivi 'Ora' per mostrare l'orologio.Dall'orologio digita cio' che vuoi e verra' mostrato sul display.");
-      return;
+        return;
       }
 
-   if (ctrlUScita == 0){   
-      Serial.print("ctr scita zero"); //Serial.println(str);
-      //myBot.getNewMessage(msg); ////// aggiunto alle 1838
-      String str = msg.text; Serial.print("MSG:TEXT"); Serial.println(str);
-      int str_len = str.length() + 1;
-      char char_array[str_len];
-
-      str.toCharArray(char_array, str_len);
-
-      strcpy (newMessage, char_array);
-
-      newMessageAvailable = true;       // aggiunto l else
-      Serial.println("sono alla fine del messaggio() " );
+      if (msg.text.equalsIgnoreCase("tastiera")) {
+        // the user is asking to show the reply keyboard --> show it
+        myBot.sendMessage(msg.sender.id, " ► TASTIERA ◄ ", myKbd);
+        isKeyboardActive = true;
+        return;
       }
 
-if (ctrlUScita == 1){   
+      else if (msg.text.equalsIgnoreCase("ud83dudd51 Orologio")) {
+        // the user is asking to show the reply keyboard --> show it
+        myBot.sendMessage(msg.sender.id, "Attivo orologio, digita qualcosa per uscire e mostrare il messaggio!");
+        Serial.println("Ahhhhhh");
+        oraTime = true;
+        // ctrlUScita = 1;   /// aggiunto per provare il panic esp8266
+        ora();
+
+      }
+
+      else if (msg.text.equalsIgnoreCase("ud83dudee0 - Officina Vicenzi -")) {
+        myBot.sendMessage(msg.sender.id, "sta scorrendo il messaggio predefinito!");
+        myBot.sendMessage(msg.sender.id, "\U0001F6E0 'OFFICINA VICENZI'");
+        ctrlUScita = 1;
+        strDaOra = "-- OFFICINA VICENZI --";
+
+      }
+
+      else if (msg.text.equalsIgnoreCase("Orologio")) {
+        // the user is asking to show the reply keyboard --> show it
+        myBot.sendMessage(msg.sender.id, "Attivo orologio, digita qualcosa per uscire e mostrare il messaggio!");
+        oraTime = true;
+        ora();
+      }
+
+      //   else if (msg.messageType == CTBotMessageLocation) {
+      //   // received a location message --> send a message with the location coordinates
+      // myBot.sendMessage(msg.sender.id, "Longitude: " + (String)msg.location.longitude +
+      // "\nLatitude: " + (String)msg.location.latitude);
+      //  }
+
+
+
+      if (ctrlUScita == 0) {
+        Serial.print("ctr scita zero");
+        String str = msg.text; Serial.print("MSG:TEXT"); Serial.println(str);
+        int str_len = str.length() + 1;
+        char char_array[str_len];
+
+        str.toCharArray(char_array, str_len);
+
+        strcpy (newMessage, char_array);
+        Serial.println("sono alla fine del messaggio() " );
+        myBot.sendMessage(msg.sender.id, "sta scorrendo il tuo messaggio: " + String(char_array) ); ////////////////////////////////
+        newMessageAvailable = true;       // aggiunto l else
+
+      }
+
+      if (ctrlUScita == 1) {
         Serial.print("ctr scita uno"); Serial.println(strDaOra);
-      //myBot.getNewMessage(msg); ////// aggiunto alle 1838
-     // String strDaOra = msg.text; 
-      Serial.print("MSG:TEXT due"); Serial.println(strDaOra);
-      int str_len = strDaOra.length() + 1;
-      char char_array[str_len];
+        Serial.print("MSG:TEXT due"); Serial.println(strDaOra);
+        int str_len = strDaOra.length() + 1;
+        char char_array[str_len];
 
-      strDaOra.toCharArray(char_array, str_len);
+        strDaOra.toCharArray(char_array, str_len);
 
-      strcpy (newMessage, char_array);
+        strcpy (newMessage, char_array);
+        Serial.println("sono alla fine del messaggio() " );
+        myBot.sendMessage(msg.sender.id, "sta scorrendo il tuo messaggio: "   + String(char_array));
+        newMessageAvailable = true;       // aggiunto l else
 
-      newMessageAvailable = true;       // aggiunto l else
-      Serial.println("sono alla fine del messaggio() " );}
+      }
 
-
-
-
-      
     }
-/// forse qua devi mettere 
-ctrlUScita = 0;
-  }
 
+    ctrlUScita = 0;
+  }
 
   if (oraTime == true) {
-    ora();
-    Serial.println("vado in ora");
-  }
 
+    Serial.println("vado in ora");
+    delay(1000);
+    ora();
+
+
+  }
 
 }
